b4eb0d49384c0efd26c9d6e0d897cf1b4761c1ff
fink <f-inc@comcast.net>
2025-05-16 16:30:40 -0400

    Fix invalid Express route pattern for options preflight

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); // Handle preflight requests
+
 // =================================================
 
 app.use(express.json());
